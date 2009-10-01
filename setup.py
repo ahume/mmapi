84e23fa0dd701219f9154eda38b4c0c5734449d4
Andy Hume <andyhume@gmail.com>
2009-10-01 12:06:37 +0100

    Added some requirements for distutils packaging.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name = 'mmapi',
+    version = '0.1',
+    description = 'Client library for the Multimap Web Service API',
+    author = 'Andy Hume',
+    download_url = 'http://github.com/ahume/mmapi/tarball/master',
+    keywords = ['geocoder', 'routing', 'spatial', 'multimap'],
+    classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Environment :: Web Environment",
+        ]
+)
